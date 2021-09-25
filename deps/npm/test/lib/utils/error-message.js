f7421750b6660602d1396054df0c64bf6d128d9d
David Neon <sdneon@users.noreply.github.com>
2021-09-25 22:48:38 +0800

    Update to Node.JS 16.10.0 baseline
    * Update to Node.JS 16.10.0 baseline
    * Update to CoffeeScript 2.6.0

--- deps/npm/test/lib/utils/error-message.js
+++ deps/npm/test/lib/utils/error-message.js
@@ -201,6 +201,17 @@ t.test('default message', t => {
   t.end()
 })
 
+t.test('args are cleaned', t => {
+  t.matchSnapshot(errorMessage(Object.assign(new Error('cmd err'), {
+    cmd: 'some command',
+    signal: 'SIGYOLO',
+    args: ['a', 'r', 'g', 's', 'https://evil:password@npmjs.org'],
+    stdout: 'stdout',
+    stderr: 'stderr',
+  }), npm))
+  t.end()
+})
+
 t.test('eacces/eperm', t => {
   const runTest = (windows, loaded, cachePath, cacheDest) => t => {
     if (windows)
@@ -418,6 +429,14 @@ t.test('404', t => {
   t.test('name with error', t => {
     const er = Object.assign(new Error('404 not found'), {
       pkgid: 'node_modules',
+      code: 'E404',
+    })
+    t.matchSnapshot(errorMessage(er, npm))
+    t.end()
+  })
+  t.test('cleans sensitive info from package id', t => {
+    const er = Object.assign(new Error('404 not found'), {
+      pkgid: 'http://evil:password@npmjs.org/not-found',
       code: 'E404',
     })
     t.matchSnapshot(errorMessage(er, npm))
